8c9a9b371602ecfc900f13f6889dbeab2e68ac03
JayanthIsTheName <jayanthisthename@gmail.com>
2025-01-19 11:47:30 +0530

    added model for lead data

--- backend/aisdr_backend/api/models.py
+++ backend/aisdr_backend/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Leads(models.Model):
+    id = models.AutoField(primary_key=True)
+    dob = models.CharField(max_length=255)
+    gstin = models.CharField(max_length=255)
+    firm_name = models.CharField(max_length=255)
+    firm_type = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    designation = models.CharField(max_length=255)
+    annual_turnover = models.CharField(max_length=255)
+    property_ownership = models.CharField(max_length=255)
+    
