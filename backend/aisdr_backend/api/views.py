2b32f34540b244a97e0bae50b1ebbaa44a5a3734
JayanthIsTheName <jayanthisthename@gmail.com>
2025-01-19 22:15:50 +0530

    added code for crud operations

--- backend/aisdr_backend/api/views.py
+++ backend/aisdr_backend/api/views.py
@@ -186,5 +186,28 @@ class VerifyOtp(APIView):
 
 
 class LeadData(APIView):
+    def get(self, request):
+        records = Leads.objects.all()
+        serializer = LeadsSerializers(records, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
     def post(self, request):
-        print('hi')
+        serializer = Leads(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request):
+        try:
+            # Delete all records from the table
+            Leads.objects.all().delete()
+            return Response(
+                {"message": "All records deleted successfully."},
+                status=status.HTTP_204_NO_CONTENT
+            )
+        except Exception as e:
+            return Response(
+                {"error": str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
